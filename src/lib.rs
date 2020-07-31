25a803ae0096f3c164c7030a8769719039d31c31
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-07-31 15:14:25 -0400

    Draw bars (#7)
    * Not so happy
    
    * Add buttons
    
    * But it doesnt update
    
    * it isnt the vec
    
    * There Is No Clock Buddy
    
    * ITER
    
    * Implement fns
    
    * Render bar with function
    
    * Draw bars
    
    * Finishu p

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,74 @@ use yew::prelude::*;
 
 struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    readings: Vec<Reading>,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Reading {
+    value: i8,
+}
+
+const MIN_READING: i8 = -3;
+const MAX_READING: i8 = 3;
+impl Reading {
+    pub fn new(value: i8) -> Reading {
+        if value < MIN_READING {
+            Reading { value: MIN_READING }
+        } else if value > MAX_READING {
+            Reading { value: MAX_READING }
+        } else {
+            Reading { value }
+        }
+    }
+    pub fn get(self) -> i8 {
+        self.value
+    }
+}
+
+fn render_bar(value: i8) -> Html {
+    html! {
+        <>
+            <div class={class_from(value, 3)}></div>
+            <div class={class_from(value, 2)}></div>
+            <div class={class_from(value, 1)}></div>
+            <div class={class_from(value, 0)}></div>
+            <div class={class_from(value, -1)}></div>
+            <div class={class_from(value, -2)}></div>
+            <div class={class_from(value, -3)}></div>
+        </>
+    }
+}
+
+fn class_from(value: i8, position: i8) -> String {
+    if position == 0 {
+        "neutral".to_string()
+    } else if position > 0 && value >= position {
+        format!("hot{}", position)
+    } else if position < 0 && value <= position {
+        format!("cold{}", position.abs())
+    } else {
+        "nocolor".to_string()
+    }
 }
 
 enum Msg {
-    AddOne,
+    AddReading(Reading),
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        Self {
+            link,
+            readings: vec![],
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1,
+            Msg::AddReading(r) => self.readings.push(r),
         }
         true
     }
@@ -35,65 +86,16 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <button onclick=self.link.callback(|_| Msg::AddReading(Reading::new(-3)))>{ "-3" }</button>
+                <button onclick=self.link.callback(|_| Msg::AddReading(Reading::new(-2)))>{ "-2" }</button>
+                <button onclick=self.link.callback(|_| Msg::AddReading(Reading::new(-1)))>{ "-1" }</button>
+                <button onclick=self.link.callback(|_| Msg::AddReading(Reading::new(0)))>{ "0" }</button>
+                <button onclick=self.link.callback(|_| Msg::AddReading(Reading::new(1)))>{ "+1" }</button>
+                <button onclick=self.link.callback(|_| Msg::AddReading(Reading::new(2)))>{ "+2" }</button>
+                <button onclick=self.link.callback(|_| Msg::AddReading(Reading::new(3)))>{ "+3" }</button>
+
                 <div id="grid">
-                    // day 1
-                    <div class="hot3"></div>
-                    <div class="hot2"></div>
-                    <div class="hot1"></div>
-                    <div class="neutral"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    // day 2
-                    <div class="nocolor"></div>
-                    <div class="hot2"></div>
-                    <div class="hot1"></div>
-                    <div class="neutral"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    // day 3
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="hot1"></div>
-                    <div class="neutral"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    // day 4
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="neutral"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    // day 5
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="neutral"></div>
-                    <div class="cold1"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    // day 6
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="neutral"></div>
-                    <div class="cold1"></div>
-                    <div class="cold2"></div>
-                    <div class="nocolor"></div>
-                    // day 7
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="nocolor"></div>
-                    <div class="neutral"></div>
-                    <div class="cold1"></div>
-                    <div class="cold2"></div>
-                    <div class="cold3"></div>
+                   { self.readings.iter().map(|r| render_bar(r.get())).collect::<Html>() }
                 </div>
             </div>
         }
