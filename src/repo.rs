84dc5d1f0fc610c9f089cef668394416043b5b5d
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-08-03 06:04:02 -0400

    write save fn

--- src/repo.rs
+++ src/repo.rs
@@ -1,25 +1,40 @@
 use crate::{MoodReading, TextSubmission};
+use web_sys::window;
 
 const MOOD_READINGS_KEY: &str = "mood_readings";
 const NOTES_KEY: &str = "notes";
 const SLEEP_KEY: &str = "sleep";
 
+fn save(key: &str, data: &str) -> Result<(), SaveErr> {
+    if let Some(w) = window() {
+        if let Ok(Some(storage)) = w.local_storage() {
+            storage
+                .set_item(MOOD_READINGS_KEY, data)
+                .map_err(|_| SaveErr)
+        } else {
+            Err(SaveErr)
+        }
+    } else {
+        Err(SaveErr)
+    }
+}
+
 fn save_mood_readings(all: &[MoodReading]) -> Result<(), SaveErr> {
-    todo!()
+    save(MOOD_READINGS_KEY, todo!())
 }
 fn load_mood_readings() -> Result<Vec<MoodReading>, LoadErr> {
     todo!()
 }
 
 fn save_notes(all: &[TextSubmission]) -> Result<(), SaveErr> {
-    todo!()
+    save(NOTES_KEY, todo!())
 }
 fn load_notes() -> Result<Vec<TextSubmission>, LoadErr> {
     todo!()
 }
 
 fn save_sleep(all: &[TextSubmission]) -> Result<(), SaveErr> {
-    todo!()
+    save(SLEEP_KEY, todo!())
 }
 fn load_sleep() -> Result<Vec<TextSubmission>, LoadErr> {
     todo!()
