b0664406e82460e632b4c37fc54157d36dea8a1b
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-08-12 05:53:33 -0400

    try an offset

--- src/web/time.rs
+++ src/web/time.rs
@@ -7,3 +7,8 @@ pub fn local_datetime(epoch_millis_utc: u64) -> DateTime<FixedOffset> {
 pub fn utc_now() -> u64 {
     js_sys::Date::now() as u64
 }
+
+const JS_CHRONO_OFFSET_COEFF: u64 = 10;
+fn local_offset_seconds() -> u64 {
+    js_sys::Date::new_0().get_timezone_offset() as u64 * JS_CHRONO_OFFSET_COEFF
+}
