7989c34e19e8f6f2563a38bf702ca64553d1fee8
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-08-13 07:57:23 -0400

    Format datetime

--- src/web/export.rs
+++ src/web/export.rs
@@ -1,8 +1,11 @@
+use crate::web::time::utc_now;
 use crate::web::StorageState;
+use chrono::prelude::*;
 use yew::prelude::*;
 
 pub fn button(storage_state: &StorageState) -> Html {
-    let formatted_datetime: String = todo!("formatted datetime");
+    let dt = Utc.timestamp_millis(utc_now() as i64);
+    let formatted_datetime: String = dt.format("%Y%m%d_%H%M%SZ").to_string();
     let filename: String = format!("equanimity_{}.json", formatted_datetime);
     if let Ok(href) = provide_data(storage_state) {
         html! { <button class="thick"><a href=href download=filename>{ "Export 💾" }</a></button> }
