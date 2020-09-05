dad897a5d383d7779e83bf978f6ecbbaa62db6d7
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-09-05 05:09:55 -0400

    Use group_by dep (#71)
    * Dedup code via crate

--- src/moods.rs
+++ src/moods.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use chrono::prelude::{DateTime, FixedOffset};
+use group_by::group_by;
 
 const DAYS_TO_DISPLAY: u8 = 14;
 
@@ -67,25 +68,6 @@ fn wildest(readings: &Vec<&MoodReading>) -> HighLowMoods {
         (None, _, Some(h)) => HighLowMoods::One(h),
         (Some(l), _, Some(h)) => HighLowMoods::MaxMin(h, l),
     }
-}
-
-fn group_by<I, F, K, T>(xs: I, mut key_fn: F) -> Vec<(K, Vec<T>)>
-where
-    I: IntoIterator<Item = T>,
-    F: FnMut(&T) -> K,
-    K: Eq,
-{
-    let mut groups = Vec::<(K, Vec<T>)>::new();
-    for item in xs {
-        let key = key_fn(&item);
-        let last = groups.last_mut();
-        if let Some((_, group)) = last.filter(|(k, _)| k == &key) {
-            group.push(item);
-        } else {
-            groups.push((key, vec![item]));
-        }
-    }
-    groups
 }
 
 #[cfg(test)]
