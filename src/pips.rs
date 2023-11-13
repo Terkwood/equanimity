dea1da74d943b94fbd89ea3dbe4fd8585f775b1d
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-11-13 16:34:56 -0500

    stub

--- src/pips.rs
+++ src/pips.rs
@@ -1,7 +1,11 @@
 use crate::*;
 
 fn draw(v: &[MoodReading]) -> String {
-    panic!("dedupe days");
-
+    let by_day = group_by_day(v);
+    
     panic!("build string");
+}
+
+fn group_by_day(v: &[MoodReading]) -> Vec<MoodReading> {
+    panic!("group by day");
 }
