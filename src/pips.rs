d5384f4a3014dfd15daad6d18e4aaa367bfe34fc
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-11-13 16:45:30 -0500

    impl test

--- src/pips.rs
+++ src/pips.rs
@@ -1,11 +1,68 @@
 use crate::*;
 
-fn draw(v: &[MoodReading]) -> String {
+pub fn draw(v: &[MoodReading]) -> String {
     let by_day = group_by_day(v);
     
-    panic!("build string");
+    let mut s = String::new();
+    for mr in by_day {
+        s.push_str(&draw_one(&mr));
+    }
+
+    s
 }
 
+/// but it will return two readings on a given day
+/// if it needs to print both manic and depressive
 fn group_by_day(v: &[MoodReading]) -> Vec<MoodReading> {
     panic!("group by day");
+}
+
+fn draw_one(mr: &MoodReading) -> String {
+   let s = format!("{}{}{}", manic_pips(mr), equanimity_pip(mr), depressive_pips(mr));
+   s
+}
+
+fn equanimity_pip(mr: &MoodReading) -> String {
+    if mr.value == 0 {
+        "⚪".to_string()
+    } else {
+        "⚫".to_string()
+    }
+}
+
+fn manic_pips(mr: &MoodReading) -> String {
+    let mut s = String::new();
+    for _ in 0..mr.value.abs() {
+        s.push_str("🔴");
+    }
+    for _ in mr.value.abs()..3 {
+        s.push_str("⚫");
+    }
+    let r = s.chars().rev().collect();
+    r
+}
+fn depressive_pips (mr: &MoodReading) -> String {
+    let mut s = String::new();
+    for _ in 0..mr.value.abs() {
+        s.push_str("🔵");
+    }
+    for _ in mr.value.abs()..3 {
+        s.push_str("⚫");
+    }
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_draw_one() {
+        let mr = MoodReading {
+            epoch_millis: 0,
+            value: 0,
+        };
+        let s = draw_one(&mr);
+        assert_eq!(s, "⚫⚫⚫⚪⚫⚫⚫");
+    }
 }
