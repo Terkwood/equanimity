f0c3a2e8c89a96a2dabc8bf36f2a8675f5146fc5
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-11-16 19:34:49 -0500

    fix: no local time

--- src/web/time.rs
+++ src/web/time.rs
@@ -3,18 +3,11 @@ use chrono::prelude::*;
 /// Note the bias to western timezone.
 /// If this software were open-sourced, we'd want to make
 /// this usable for individuals in the eastern hemisphere.
-pub fn js_local_datetime(epoch_millis_utc: u64) -> DateTime<FixedOffset> {
-    let offset = FixedOffset::west_opt(local_offset_seconds()).expect("offset");
+pub fn js_utc_datetime(epoch_millis_utc: u64) -> DateTime<Utc> {
     Utc.timestamp_millis_opt(epoch_millis_utc as i64)
         .unwrap()
-        .with_timezone(&offset)
 }
 
 pub fn utc_now() -> u64 {
     js_sys::Date::now() as u64
-}
-
-const JS_CHRONO_OFFSET_COEFF: i32 = 60;
-fn local_offset_seconds() -> i32 {
-    js_sys::Date::new_0().get_timezone_offset() as i32 * JS_CHRONO_OFFSET_COEFF
 }
