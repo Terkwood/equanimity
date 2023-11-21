f316a58648c4eaa9e73e49e54b40a0c020d9b4b6
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-11-21 15:15:09 -0500

    fix: trim lies from comments!

--- src/web/time.rs
+++ src/web/time.rs
@@ -1,8 +1,5 @@
 use chrono::prelude::*;
 
-/// Note the bias to western timezone.
-/// If this software were open-sourced, we'd want to make
-/// this usable for individuals in the eastern hemisphere.
 pub fn js_utc_datetime(epoch_millis_utc: u64) -> DateTime<Utc> {
     Utc.timestamp_millis_opt(epoch_millis_utc as i64).unwrap()
 }
