84d8ab094e73ddaa7ba9497e2fdf4773e79d160d
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-11-23 17:42:15 -0500

    stored

--- src/repo.rs
+++ src/repo.rs
@@ -1,47 +1,37 @@
 use crate::{MoodReading, TextSubmission, TextType};
 //use yew::format::Json;
 use gloo::storage::LocalStorage;
-// use yew StorageService
-
+use gloo_storage::{Storage, errors::StorageError};
 
 const MOOD_READINGS_KEY: &str = "mood_readings";
 const NOTES_KEY: &str = "notes";
 const MEDS_KEY: &str = "meds";
 const SLEEP_KEY: &str = "sleep";
 
-/// Yew wrapper around (in our case, local) storage.
-/// See https://github.com/yewstack/yew/issues/1287#issuecomment-671043231
-pub struct YewRepo {
-    storage: StorageService,
-}
 
-impl YewRepo {
-    pub fn save_mood_readings(&mut self, all: &Vec<MoodReading>) -> Result<(), SaveErr> {
-        let value = Json(all);
-        Ok(self.storage.store(MOOD_READINGS_KEY, value))
+    pub fn save_mood_readings( all: &Vec<MoodReading>) -> Result<(), StorageError> {
+        LocalStorage::set(MOOD_READINGS_KEY, serde_json::to_string(all).expect("readings to string"))
     }
 
     pub fn save_text(
-        &mut self,
         text_type: TextType,
         all: &Vec<TextSubmission>,
-    ) -> Result<(), SaveErr> {
-        let value = Json(all);
-        Ok(self.storage.store(text_key(text_type), value))
+    ) -> Result<(), StorageError> {
+        LocalStorage::set(text_key(text_type), serde_json::to_string(all).expect("text subs to string"))
     }
 
-    pub fn load_mood_readings(&self) -> Result<Vec<MoodReading>, LoadErr> {
+    pub fn load_mood_readings() -> Result<Vec<MoodReading>, LoadErr> {
         Ok(
-            if let Json(Ok(restored_model)) = self.storage.restore(MOOD_READINGS_KEY) {
+            if let Ok(restored_model) = LocalStorage::get(MOOD_READINGS_KEY) {
                 restored_model
             } else {
                 Vec::new()
             },
         )
     }
-    pub fn load_text(&self, text_type: TextType) -> Result<Vec<TextSubmission>, LoadErr> {
+    pub fn load_text( text_type: TextType) -> Result<Vec<TextSubmission>, LoadErr> {
         Ok(
-            if let Json(Ok(restored_model)) = self.storage.restore(text_key(text_type)) {
+            if let Ok(restored_model) = LocalStorage::get(text_key(text_type)) {
                 restored_model
             } else {
                 Vec::new()
@@ -49,12 +39,6 @@ impl YewRepo {
         )
     }
 
-    pub fn new() -> Self {
-        let storage = StorageService::new(Area::Local).expect("storage was disabled by the user");
-
-        Self { storage }
-    }
-}
 
 fn text_key(text_type: TextType) -> &'static str {
     match text_type {
