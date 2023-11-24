57a975daaf5c8e6b3d47b16557dae0fa9c1d18f7
terkwood <38859656+Terkwood@users.noreply.github.com>
2023-11-24 12:53:45 -0500

    dress it up

--- src/web/backdate.rs
+++ src/web/backdate.rs
@@ -1,30 +1,54 @@
-use yew::Component;
-use yew_datepicker::Datepicker;
+use chrono::NaiveDate;
+use yew::Component; 
 use crate::*;
+use yew_datepicker::Datepicker;
 
 pub struct BackdateMoodReadings {
+    pub current_date: Option<NaiveDate>,
+    pub request_user_pick_date: bool,
+}
 
+pub enum BackdateMsg {
+    DateSelected(NaiveDate),
+    BackdateReading(MoodReading),
 }
 
 impl Component for BackdateMoodReadings {
-    type Message = ();
+    type Message = BackdateMsg;
     type Properties = ();
 
     fn create(_: &Context<Self>) -> Self {
-        Self {}
+        Self { current_date: None, request_user_pick_date: false }
     }
 
-    fn update(&mut self, _: &Context<Self>, _: Self::Message) -> bool {
+    fn update(&mut self, _: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            BackdateMsg::DateSelected(naive_date) => {
+                self.current_date = Some(naive_date);
+            }
+            BackdateMsg::BackdateReading(reading) => {
+                unimplemented!("Backdate reading");
+            }
+        }
+        
         true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <Datepicker on_select={ ctx.link()
-                .callback(|naive_date| 
-                // emit date using web_sys console.log
-                web_sys::console::log_1(&format!("Date selected: {}", naive_date).into()
-  )               )}/>
+            <>
+                <Datepicker on_select={ ctx.link()
+                    .callback(|naive_date| 
+                        BackdateMsg::DateSelected(naive_date))}/>
+
+                { if self.request_user_pick_date {
+                    html! {
+                        <p> {"Please pick a date."} </p>
+                    }
+                } else {
+                    html! {</>}
+                }
+            </>
         }
     }
 }
