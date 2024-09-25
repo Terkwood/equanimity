d8ad738ada26aa72f00dc6bc6499db9a5594c31a
Elaine Terkhorn <fterkhorn@salesforce.com>
2024-09-25 07:06:13 -0400

    model

--- src/web/quick_med.rs
+++ src/web/quick_med.rs
@@ -2,3 +2,9 @@ use crate::*;
 use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
 use yew::Component;
 
+pub struct LogQuickMed {
+    pub choice: QuickMedChoice,
+    pub current_time: NaiveDateTime,
+}
+
+pub struct QuickMedChoice {}
