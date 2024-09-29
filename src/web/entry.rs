cdaa2ab9a50e3557703b4cb60e34322234f38be8
Elaine Terkhorn <fterkhorn@salesforce.com>
2024-09-29 14:00:23 -0400

    entry

---
+++ src/web/entry.rs
@@ -0,0 +1,30 @@
+
+#[derive(Eq, PartialEq, Clone, Debug)]
+pub enum Entry {
+    Mood(MoodReading),
+    Sleep(TextSubmission),
+    Meds(TextSubmission),
+    Note(TextSubmission),
+}
+
+impl Entry {
+    pub fn timestamp(&self) -> u64 {
+        match self {
+            Entry::Mood(m) => m.epoch_millis,
+            Entry::Sleep(t) => t.epoch_millis,
+            Entry::Meds(m) => m.epoch_millis,
+            Entry::Note(t) => t.epoch_millis,
+        }
+    }
+}
+impl PartialOrd for Entry {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Entry {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.timestamp().cmp(&other.timestamp())
+    }
+}
