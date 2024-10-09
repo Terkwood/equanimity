ea6dc5182901e6da459b2df6e4d245e7d949573b
Elaine Terkhorn <fterkhorn@salesforce.com>
2024-10-09 17:53:55 -0400

    ok

--- src/web/about.rs
+++ src/web/about.rs
@@ -1,22 +1,9 @@
 use std::{future::IntoFuture, str::FromStr};
 
-use super::logs::Logs;
 use crate::*;
-use futures::TryFutureExt;
 use gloo::utils::format::JsValueSerdeExt;
-use js_sys::Promise;
 use web::storage_state::StorageState;
 use web_sys::console;
-use yew::{prelude::*, virtual_dom::VNode};
-use yew_export_button::{export_button, ButtonOpts};
-
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-const REPO_URL: &str = "https://github.com/Terkwood/equanimity";
-
-const EXPORT_BUTTON_CSS_ID: &str = "export-button";
-const EXPORT_LINK_CSS_CLASS: &str = "fancy-button thick";
-const EXPORT_FILE_PREFIX: &str = "equanimity";
-
 pub async fn on_click_import(e: web_sys::MouseEvent) -> Result<JsValue, JsValue> {
     let import_p = web_sys::window()
         .expect("no global window")
@@ -24,12 +11,17 @@ pub async fn on_click_import(e: web_sys::MouseEvent) -> Result<JsValue, JsValue>
     match import_p {
         Ok(promise) => {
             let result = wasm_bindgen_futures::JsFuture::from(promise).await?;
-            let deser: Result<StorageState, _> = result.into_serde();
+            let deser: Result<TestJson, _> = result.into_serde();
             match deser {
-                Err(_e) => console::error_1(&"deser error".into()),
-                Ok(_) => console::log_1(&"IT WORKED".into()),
+                Err(_e) => { 
+                    console::error_1(&"deser error".into());
+                    Err(js_sys::JsString::from_str("It failed to deser").expect("js str").into())
+                
+                 },
+                Ok(_) => { console::log_1(&"IT WORKED".into());
+                    Ok(js_sys::JsString::from_str("Nicely done").expect("js str").into())
+                },
             }
-            unimplemented!()
         }
         Err(_j) => {
             web_sys::console::error_1(&"error import".into());
@@ -37,3 +29,6 @@ pub async fn on_click_import(e: web_sys::MouseEvent) -> Result<JsValue, JsValue>
         }
     }
 }
+
+#[derive(Deserialize)]
+pub struct TestJson { pub test: String }
